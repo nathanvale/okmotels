8222819eb5f681436ae4b59d055e168ea1fb2bac
Nathan <nathan.vale@origin.com.au>
2019-07-15 15:38:58 +1000

    chore: wip

--- .circleci/headless_configure_project.sh
+++ .circleci/headless_configure_project.sh
@@ -29,7 +29,7 @@ PROVIDERS="{\
 \"awscloudformation\":$AWSCLOUDFORMATIONCONFIG\
 }"
 
-amplify configure project \
+npx amplify configure project \
 --amplify $AMPLIFY \
 --frontend $FRONTEND \
 --providers $PROVIDERS \
